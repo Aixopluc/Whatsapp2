21ce808bcd623cc74402de311657fe6c3fa96898
Oriol Caro Bossa <ocarobossa@gmail.com>
2023-06-06 20:20:48 +0200

    Ejmerge2

--- src/ejmerge.java
+++ src/ejmerge.java
@@ -2,7 +2,7 @@ public class ejmerge {
     public static void main(String[]args){
         System.out.println("CAMBIO PARA PODER HACER MERGE");
         System.out.println("mas cambio");
-
+        System.out.println("otro cambio para hacerle un commit");
         System.out.println("otro cambio para hacerle un commit");
     }
 }
